d19e75a47e11350aa462ae4979fbf551a8f67a6f
MaximEmelyanchenko <beta3-z@mail.ru>
2018-11-10 19:17:50 +0300

    Chapter 4

--- src/Dollar.java
+++ src/Dollar.java
@@ -1,11 +1,17 @@
 public class Dollar {
-    int amount;
+    private int amount;
 
     Dollar(int amount){
         this.amount = amount;
     }
 
-    public Dollar times(int multiplier){
+    Dollar times(int multiplier){
         return new Dollar(amount * multiplier);
+    }
+
+    public boolean equals(Object object){
+        Dollar dollar = (Dollar) object;
+
+        return amount == dollar.amount;
     }
 }
